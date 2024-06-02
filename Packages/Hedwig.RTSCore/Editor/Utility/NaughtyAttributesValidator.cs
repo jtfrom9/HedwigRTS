a4c4ebb01507ceb308f43f5f5ead7d11a02eb791
jtfrom9 <jun.tachikawa@gmail.com>
2024-06-02 13:58:25 +0900

    NaughtyAttributesValidator to raise Error on Play Start, by checking if Required field value is null or not

---
+++ Packages/Hedwig.RTSCore/Editor/Utility/NaughtyAttributesValidator.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Linq;
+using UnityEngine;
+using UnityEditor;
+using NaughtyAttributes;
+
+namespace Hedwig.Editor
+{
+    [InitializeOnLoad]
+    public static class PlayModeValidator
+    {
+        static PlayModeValidator()
+        {
+            EditorApplication.playModeStateChanged += OnPlayModeStateChanged;
+        }
+
+        private static void OnPlayModeStateChanged(PlayModeStateChange state)
+        {
+            if (state == PlayModeStateChange.ExitingEditMode || state==PlayModeStateChange.EnteredPlayMode)
+            {
+                if (!ValidateRequiredFields())
+                {
+                    Debug.LogError("Play mode cannot start because there are unassigned required fields.");
+                    EditorApplication.isPlaying = false;
+                }
+            }
+        }
+
+        private static bool ValidateRequiredFields()
+        {
+            bool isValid = true;
+
+            var scriptableObjects = Resources.FindObjectsOfTypeAll<ScriptableObject>();
+
+            foreach (var so in scriptableObjects)
+            {
+                var fields = so.GetType().GetFields(System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.Public);
+
+                foreach (var field in fields)
+                {
+                    if (Attribute.IsDefined(field, typeof(RequiredAttribute)))
+                    {
+                        var value = field.GetValue(so);
+
+                        if (value == null || value is UnityEngine.Object unityObject && unityObject == null)
+                        {
+                            Debug.LogError($"The field '{field.Name}' in '{so.name}' ({so.GetType().Name}) is required but not assigned.", so);
+                            isValid = false;
+                        }
+                    }
+                }
+            }
+
+            return isValid;
+        }
+    }
+
+}
